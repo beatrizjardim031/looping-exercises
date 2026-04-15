5d81ebc78b3aa25fe93a4f076683d367cc3d81f9
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-15 10:56:31 -0500

    Initial commit: While loops exercise

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+//      while loop evaluates the condition before running the loop body
+
+        int numTimes = 0;
+
+        while (numTimes <= 5) {
+            System.out.println("I like Java.");
+            numTimes += 1;
+
+        }
+    }
+}
